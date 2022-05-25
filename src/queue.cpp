1b073a34963a4c5387a109ec29ba07e1fa0a7c9e
Everton Cavalcante <evertonranielly@gmail.com>
2022-05-25 17:03:21 -0300

    Implementação da demonstração de queue

---
+++ src/queue.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file	queue.cpp
+ * @brief	Demonstracao do uso do container <code>queue</code>
+ * @author	Everton Cavalcante (everton.cavalcante@ufrn.br)
+ * @since	23/05/2022
+ * @date	25/05/2022
+ * @sa      http://www.cplusplus.com/reference/queue/queue/
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <queue>
+using std::queue;
+
+#include <string>
+using std::string;
+
+/**
+ * @brief Função genérica para imprimir os elementos de uma fila
+ * @tparam T Tipo genérico
+ * @param q Fila
+ */
+template <typename T>
+void imprime(queue<T> q) {
+    cout << "Elementos da fila:" << endl;
+    while (!q.empty()) {
+        cout << q.front();
+        if (q.size() != 1) {
+            cout << ", ";
+        }
+        q.pop();
+    }
+    cout << endl;
+}
+
+/** @brief Funcao principal */
+int main() {
+    queue<string> q;
+    q.push("Roberto");
+    q.push("Antonio");
+    q.push("Maria");
+
+    imprime(q);
+    return 0;
+}
