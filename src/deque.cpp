4d5965bd5236d3c0c0ced1bb7bdcf65a7c3027c9
Everton Cavalcante <evertonranielly@gmail.com>
2022-05-25 17:09:55 -0300

    Implementação da demonstração de deque

---
+++ src/deque.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file	deque.cpp
+ * @brief	Demonstracao do uso do container <code>deque</code>
+ * @author	Everton Cavalcante (everton.cavalcante@ufrn.br)
+ * @since	23/05/2022
+ * @date	25/05/2022
+ * @sa      http://www.cplusplus.com/reference/deque/deque/
+ */
+
+#include <deque>
+using std::deque;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+/**
+ * @brief Função genérica para imprimir os elementos de uma fila
+ * @tparam T Tipo genérico
+ * @param d Deque
+ */
+template <typename T>
+void imprime(deque<T> d) {
+    cout << "Elementos do deque:" << endl;
+    for (deque<int>::iterator it = d.begin(); it != d.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+/** @brief Funcao principal */
+int main() {
+    deque<int> d;
+    for (int i = 5; i > 0; i--) {
+        d.push_front(i);                // Inserção de elementos no início (0-5)
+    }
+    for (int i = 6; i < 11; i++) {
+        d.push_back(i);                 // Inserção de elementos no final (6-10)
+    }
+
+    d.pop_back();                       // Remoção do último elemento do deque
+    d.pop_front();                      // Remoção do primeiro elemento do deque
+
+    imprime(d);
+    return 0;
+}
